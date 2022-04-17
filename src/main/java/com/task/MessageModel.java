f16efd8b10071c41b08a8565ee4264c483fd29f1
anhthin102 <18521434@gm.uit.edu.vn>
2022-04-17 15:20:49 +0700

    update

---
+++ src/main/java/com/task/MessageModel.java
@@ -0,0 +1,26 @@
+package com.task;
+
+public class MessageModel {
+    public MessageModel(){
+        DataGenerator d = new DataGenerator();
+        this.Id = d.Id();
+        this.Quantity=d.Quantity();
+
+    }
+    public int Id;
+    public int Quantity;
+
+    @Override
+    public String toString() {
+
+        return "{Id= "+ Id + "," +
+                "Quantity= "+Quantity+"}";
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof MessageModel &&
+                this.Id == ((MessageModel) other).Id;
+    }
+
+}
