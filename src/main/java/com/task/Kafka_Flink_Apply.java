125be209c122e2cdd74a7dd25a156b09bfec8ec3
anhthin102 <18521434@gm.uit.edu.vn>
2022-04-17 21:59:18 +0700

    update

--- src/main/java/com/task/Kafka_Flink_Apply.java
+++ src/main/java/com/task/Kafka_Flink_Apply.java
@@ -4,6 +4,9 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 
@@ -17,12 +20,20 @@ public class Kafka_Flink_Apply {
     public static void StreamStringOperation(ParameterTool parameterTool ) throws Exception {
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
         FlinkKafkaConsumer<String> flinkKafkaConsumer = createStringConsumerForTopic(parameterTool);
+        FlinkKafkaProducer<String> flinkKafkaProducer = createStringProducer(parameterTool);
         DataStream<String> stringInputStream = environment.addSource(flinkKafkaConsumer);
 //        stringInputStream.print();
         DataStream<MessageModel> message = stringInputStream.flatMap(new Tokenizer ());
         message = message.flatMap(new ShowMessageModel());
+
+        DataStream<String> groupMessage= message.keyBy(new KeyByDescription())
+                .window(TumblingProcessingTimeWindows.of(org.apache.flink.streaming.api.windowing.time.Time.seconds(60)))
+                .process(new ProcessWindow());
+        groupMessage.addSink(flinkKafkaProducer);
         environment.execute();
     }
+
+
 
     public static FlinkKafkaConsumer<String> createStringConsumerForTopic(
             ParameterTool parameterTool) {
@@ -31,5 +42,11 @@ public class Kafka_Flink_Apply {
                 new SimpleStringSchema(), parameterTool.getProperties());
 
         return consumer;
+    }
+    public static FlinkKafkaProducer<String> createStringProducer(
+            ParameterTool parameterTool){
+
+        return new FlinkKafkaProducer<>(parameterTool.getRequired("bootstrap.servers"),parameterTool.getRequired("output-topic"),
+                new SimpleStringSchema());
     }
 }
